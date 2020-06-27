bfd09c36d7e007ec58f537d0b3153007c5fa01f1
King Kenway <kingkenway16@gmail.com>
2020-06-27 04:25:31 +0100

    Initial push

---
+++ languages/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Language, Paradigm, Programmer
+
+# Register your models here.
+admin.site.register(Language)
+admin.site.register(Paradigm)
+admin.site.register(Programmer)
